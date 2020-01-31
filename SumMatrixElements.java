8d55cbf32879f3543c88dae56eab20c14f9e79c7
Mladen Petrov <mladen.petrov88@gmail.com>
2020-01-31 11:25:51 +0200

    Add files via upload

---
+++ SumMatrixElements.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class SumMatrixElements {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] input = scanner.nextLine().split(",\\s+");
+        int rows = Integer.parseInt(input[0]);
+        int cols = Integer.parseInt(input[1]);
+
+        int[][] matrix = new int[rows][cols];
+
+        for (int row = 0; row < matrix.length; row++) {
+            String[] line = scanner.nextLine().split(",\\s+");
+            for (int col = 0; col < matrix[row].length; col++) {
+                matrix[row][col] = Integer.parseInt(line[col]);
+            }
+        }
+
+        int sum = 0;
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                sum += matrix[row][col];
+            }
+        }
+        System.out.println(rows);
+        System.out.println(cols);
+        System.out.println(sum);
+    }
+}
